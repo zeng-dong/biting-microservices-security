a1ac59da2fe9a2a15bb64a3caf0333619bec7556
Dong Zeng <dong@freshbytesoftware.com>
2021-04-08 10:22:45 -0500

    setup ClientCredentials flow for EventCatalogService on the web application (the client)

--- GloboTicket.Client/Services/EventCatalogService.cs
+++ GloboTicket.Client/Services/EventCatalogService.cs
@@ -1,5 +1,6 @@
 ﻿using GloboTicket.Web.Extensions;
 using GloboTicket.Web.Models.Api;
+using IdentityModel.Client;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -10,32 +11,69 @@ namespace GloboTicket.Web.Services
     public class EventCatalogService : IEventCatalogService
     {
         private readonly HttpClient client;
+        private string _accessToken;
 
         public EventCatalogService(HttpClient client)
         {
             this.client = client;
         }
 
+        private async Task<string> GetToken()
+        {
+            if (!string.IsNullOrEmpty(_accessToken))
+            {
+                return _accessToken;
+            }
+
+            var discoveryDocumentResponse = await client.GetDiscoveryDocumentAsync("https://localhost:5010");
+            if (discoveryDocumentResponse.IsError)
+            {
+                throw new Exception(discoveryDocumentResponse.Error);
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                    new ClientCredentialsTokenRequest
+                    {
+                        Address = discoveryDocumentResponse.TokenEndpoint,
+                        ClientId = "globoticketm2m",
+                        ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+                        Scope = "globoticket.fullaccess"
+                    }
+                );
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception(tokenResponse.Error);
+            }
+
+            _accessToken = tokenResponse.AccessToken;
+            return _accessToken;
+        }
+
         public async Task<IEnumerable<Event>> GetAll()
         {
+            client.SetBearerToken(await GetToken());
             var response = await client.GetAsync("/api/events");
             return await response.ReadContentAs<List<Event>>();
         }
 
         public async Task<IEnumerable<Event>> GetByCategoryId(Guid categoryid)
         {
+            client.SetBearerToken(await GetToken());
             var response = await client.GetAsync($"/api/events/?categoryId={categoryid}");
             return await response.ReadContentAs<List<Event>>();
         }
 
         public async Task<Event> GetEvent(Guid id)
         {
+            client.SetBearerToken(await GetToken());
             var response = await client.GetAsync($"/api/events/{id}");
             return await response.ReadContentAs<Event>();
         }
 
         public async Task<IEnumerable<Category>> GetCategories()
         {
+            client.SetBearerToken(await GetToken());
             var response = await client.GetAsync("/api/categories");
             return await response.ReadContentAs<List<Category>>();
         }
