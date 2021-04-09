78132bd5e8d940bbb18d1a4c5102bf1ec26f0c50
Dong Zeng <dong@freshbytesoftware.com>
2021-04-09 14:05:27 -0500

    add logout - from both application and identity service

---
+++ GloboTicket.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
